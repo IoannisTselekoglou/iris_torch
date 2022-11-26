b9439a2cda5b7a90b6cc415d0f5aae86a1413cbe
IoannisTselekoglou <janni.tselekoglou@googelmail.com>
2022-11-26 18:13:52 +0100

    fixed accuracy_model.p

--- accuracy_model.py
+++ accuracy_model.py
@@ -10,16 +10,16 @@ import torch
 class data_loader:
 
     # tensor[0:,1] = 4 to accses specific value 
-    def accuracy(model, test_dataset, batch_size: int):
+    def accuracy(model,test_loader, batch_size: int):
         summe = 0
         def transform_label(label_data):
             data = []
             for i in label_data:
                 if i == "Iris-setosa":
                     data.append(torch.tensor([1,0,0], dtype=torch.float32))
-                elif i == "Iris-versicolor": 
+                if i == "Iris-versicolor":
                     data.append(torch.tensor([0,1,0], dtype=torch.float32))
-                else:
+                if i == "Iris-virginica":
                     data.append(torch.tensor([0,0,1], dtype=torch.float32))
             return torch.stack(data)
 
@@ -31,8 +31,8 @@ class data_loader:
                 list_tensors.append(final_tensor)
             return torch.cat(list_tensors)
 
-        test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
-        for i,(X_test, test_labels) in enumerate(test_loader):
+        for i in range(3):
+            X_test, test_labels = next(iter(test_loader))
             test_labels = transform_label(test_labels)
             x_label_pre = model(X_test)
             _, x_label_pre_hat = torch.max(x_label_pre, 1)
@@ -40,17 +40,17 @@ class data_loader:
             idx = 0
             number_pred = 0
             counter = 0
+            #print(model_prediction, test_labels)
             while idx < 10:
-                print(f" \n Tensor[{idx+1}]")
-                for i in range(3):
-                    print(f"model: {model_prediction[idx][i].item()}, label: {test_labels[idx][i].item()}")
-                    if model_prediction[idx][i].item() == test_labels[idx][i].item():
+                for j in range(3):
+                    if model_prediction[idx][j].item() == test_labels[idx][j].item():
                         counter += 1
                         if counter == 3:
                             number_pred += 1
-                    counter = 0
+                counter = 0
                 idx +=1
-            accuracy_per_epoch = number_pred/10 *100
-        summe += accuracy_per_epoch
-        return print(f"\naccuracy of model {summe/iters}%")
+            accuracy_per_epoch = (number_pred/10)*100
+            print(f"accuracy per batch {i}:\n{accuracy_per_epoch}%")
+            summe += accuracy_per_epoch
+        return print(f"\ntotal accuracy of model {(summe/len(test_loader)):.2f}%")
 
